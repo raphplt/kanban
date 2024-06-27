9fd6f63949420baf4f27bb80934727cfbc3bab07
raphaeltkorp <raphael@tkorp.com>
2024-06-27 12:41:33 +0200

    chore: Remove unused code and dependencies

--- src/components/KanbanBoard.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { DndContext, rectIntersection } from "@dnd-kit/core";
-import { useState } from "react";
-
-export default function KanbanBoard() {
-	return <DndContext collisionDetection={rectIntersection}></DndContext>;
-}
